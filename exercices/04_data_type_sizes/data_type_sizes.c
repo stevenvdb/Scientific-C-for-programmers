fab99f3a1f5924d85f3b65020891aaf8f6adc13f
Steven Vandenbrande <steven.vandenbrande@kuleuven.be>
2025-10-28 13:05:06 +0100

    Complete first version of presentation

--- exercices/04_data_type_sizes/data_type_sizes.c
+++ exercices/04_data_type_sizes/data_type_sizes.c
@@ -6,11 +6,11 @@
 
 int main() {
 
-    printf("The size of char is %ld bytes; it ranges from %d to %d\n",
+    printf("The size of char is %zu bytes; it ranges from %d to %d\n",
            sizeof(char), CHAR_MIN, CHAR_MAX);
-    printf("The size of int is %ld bytes; it ranges from %d to %d\n",
+    printf("The size of int is %zu bytes; it ranges from %d to %d\n",
            sizeof(int), INT_MIN, INT_MAX);
-    printf("The size of uint32_t is %ld bytes\n", sizeof(int16_t));
+    printf("The size of uint32_t is %zu bytes\n", sizeof(int16_t));
 
     printf("The largest double that can be represented is %6.1e\n", DBL_MAX);
     printf("The smallest positive double that can be represented is %6.1e\n", DBL_MIN);
--- exercices/04_data_type_sizes/solution/data_type_sizes.c
+++ exercices/04_data_type_sizes/solution/data_type_sizes.c
@@ -6,13 +6,13 @@
 
 int main() {
 
-    printf("The size of char is %ld bytes; it ranges from %d to %d\n",
+    printf("The size of char is %zu bytes; it ranges from %d to %d\n",
            sizeof(char), CHAR_MIN, CHAR_MAX);
-    printf("The size of int is %ld bytes; it ranges from %d to %d\n",
+    printf("The size of int is %zu bytes; it ranges from %d to %d\n",
            sizeof(int), INT_MIN, INT_MAX);
-    printf("The size of unsigned long int is %ld bytes; its max is %lu\n",
+    printf("The size of unsigned long int is %zu bytes; its max is %lu\n",
            sizeof(unsigned long int), ULONG_MAX);
-    printf("The size of uint32_t is %ld bytes\n", sizeof(int16_t));
+    printf("The size of uint32_t is %zu bytes\n", sizeof(int16_t));
 
     printf("The largest double that can be represented is %6.1e\n", DBL_MAX);
     printf("The smallest positive double that can be represented is %6.1e\n", DBL_MIN);
