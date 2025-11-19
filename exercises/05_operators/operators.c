71d3c6d3a3cea2b30c1fafe3b910486b6d417969
Steven Vandenbrande <steven.vandenbrande@kuleuven.be>
2025-11-19 17:16:51 +0100

    Fix operators exercise

--- exercises/05_operators/operators.c
+++ exercises/05_operators/operators.c
@@ -7,8 +7,7 @@ int isLeapYear(int year) {
        - it is divisible by 4 but not by 100 OR
        - it is disisible by 400
     */
-    if ( (year % 4 == 0 && year % 100 != 0) || (year % 400) == 0) return 1;
-    else return 0; 
+    return 0;
 }
 
 int main() {
@@ -21,13 +20,13 @@ int main() {
     // Multiply i by 8 and store it in j, but without making use of the * operator
     // Hint: use a bitwise operator 
     int i = 5;
-    int j = i << 3;
+    int j = -1;
     assert(j == 40);
 
     // Compute c as the minimal value of a and b without using an if-else statement
     int a = 126;
     int b = 45;
-    int c = (a < b) ? a : b;
+    int c = -1;
     assert(c == b);
 
     printf("All checks passed!\n");
