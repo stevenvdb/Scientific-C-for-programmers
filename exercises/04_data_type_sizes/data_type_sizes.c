960a6d937f75c5e083b8d6db56daa3bc0d95d4fe
Steven Vandenbrande <steven.vandenbrande@kuleuven.be>
2025-11-20 14:41:46 +0100

    Fix minor mistakes in session2

--- exercises/04_data_type_sizes/data_type_sizes.c
+++ exercises/04_data_type_sizes/data_type_sizes.c
@@ -10,7 +10,7 @@ int main() {
            sizeof(char), CHAR_MIN, CHAR_MAX);
     printf("The size of int is %zu bytes; it ranges from %d to %d\n",
            sizeof(int), INT_MIN, INT_MAX);
-    printf("The size of uint32_t is %zu bytes\n", sizeof(int16_t));
+    printf("The size of uint32_t is %zu bytes\n", sizeof(uint32_t));
 
     printf("The largest double that can be represented is %6.1e\n", DBL_MAX);
     printf("The smallest positive double that can be represented is %6.1e\n", DBL_MIN);
