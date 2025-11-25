2f4bccc08b4726149c8078d11c0f2f7d6b84f131
Steven Vandenbrande <steven.vandenbrande@kuleuven.be>
2025-11-25 08:39:15 +0100

    Add timings to mc_pi exercise

--- exercises/06_mc_pi/solution/mc_pi.c
+++ exercises/06_mc_pi/solution/mc_pi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 double get_random_double() {
     // Get a random double in [0, 1]
@@ -16,14 +17,22 @@ int main() {
     // This can be used to seed the random number generator
     srand(0);
 
+    struct timeval tstart, tend, walltime;
+    gettimeofday(&tstart, NULL);
     for (unsigned long i = 0; i < N; i++) {
         x = get_random_double();
         y = get_random_double();
         if ( x*x + y*y < 1 ) {
             hits++;
         }
+#ifdef PRINT_PROGRESS
         if (i % 1000000 == 0) printf("Estimated after %lu: pi = %12.8f\n", i, 4.0 * hits / ( i + 1));
+#endif
     }
+    gettimeofday(&tend, NULL);
+    timersub(&tend, &tstart, &walltime);
+    double walltime_s = walltime.tv_sec + 1e-6 * walltime.tv_usec;
+    printf("Walltime = %8.3fs, %12.1e iterations/s\n", walltime_s, N / walltime_s);
 
     double pi = 4.0 * hits / N;
     double error = pi - M_PI;
